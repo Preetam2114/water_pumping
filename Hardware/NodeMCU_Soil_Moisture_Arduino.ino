925ea41aaca6b298d5b2d8795e3adf525797ce89
Preetam Rane <preetamrane2114@gmail.com>
2023-06-14 23:51:18 +0530

    Update NodeMCU_Soil_Moisture_Arduino.ino

--- Hardware/NodeMCU_Soil_Moisture_Arduino.ino
+++ Hardware/NodeMCU_Soil_Moisture_Arduino.ino
@@ -1,7 +1,11 @@
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <ArduinoJson.h>
 #include <SoftwareSerial.h>
 #include <TimeLib.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
 
 #define moistureSensor A0
 // #define temperatureSensor A1
@@ -11,16 +15,24 @@
 const char* ssid = "";
 const char* password = "";
 
-// Replace with your Google app script id
-String sheet_id = "AKfycbzImhI5tpPS3kZLSqDMdjRQPzd2TBP5pKzolDi6FGHcJLijD8P7m9qmVjExvFz_65Dkdw";
-
-const char* host = "script.google.com";
+// Replace with your Appwrite details
+const char* endpoint = "cloud.appwrite.io";
 const int httpsPort = 443;
+const char* project = "648826a88cadbb1a59d5";
+const char* functionId = "64884fc42de30486ac95";
+const char* appwriteKey = "9dfeba777ec0831e2eca697c89d82f4a8e7db61ee2f444e4157f4244d05e1619e8a4b9325fea3336f425dbc75b00ba814f65f3f46120c26e3235c98061cfd4b354396d8eb4ff3fbf88d251719cdb986e716e67597db4ba9cb64be11e427469b05445760cf8e89fd74db3e10003f417d69818d41ba4d632e2f8899dac361634e9";
 
-WiFiClientSecure client;
+BearSSL::WiFiClientSecure client;
 
 unsigned long previousDataTime = 0;
+unsigned long totalPumpedWater = 0;
+unsigned long pumpedWater = 0;
+unsigned long pumpStartTime = 0;
 const unsigned long dataInterval = 1000; // 1 sec delay for sending data
+const unsigned long maxWaterLimit = 1200; // 1.2 L
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 void setup() {
   Serial.begin(115200);
@@ -28,32 +40,33 @@ void setup() {
   digitalWrite(waterPump, LOW);
 
   connectToWiFi();
-  setTime(0, 0, 0, 1, 1, 2023); // Set initial time (hours, minutes, seconds, day, month, year)
+  setTime(getNtpTime()); // Set initial time to NTP synchronized time
+
+  // Update NTP time every hour
+  timeClient.begin();
+  timeClient.update();
 }
 
 void loop() {
   unsigned long currentMillis = millis();
+  int currentHourOfDay = hour();
+
+  if (currentHourOfDay >= 19) {
+    // Reset totalPumpedWater to zero after 7 PM
+    totalPumpedWater = 0;
+  }
 
   int moistureValue = analogRead(moistureSensor);
-  // int temperatureValue = analogRead(temperatureSensor);
-
   float moisturePercentage = map(moistureValue, 0, 1024, 0, 100);
   moisturePercentage = (moisturePercentage - 100) * -1;
-
-  // float temperature = map(temperatureValue, 0, 1024, 0, 500);
-  // temperature = temperature / 10.0;
 
   Serial.print("Moisture: ");
   Serial.print(moisturePercentage);
   Serial.println("%");
 
-  // Serial.print("Temperature: ");
-  // Serial.print(temperature);
-  // Serial.println("°C");
-
   bool pumpStatus = false;
-  
-  if (moisturePercentage < 10) {
+
+  if (moisturePercentage >= 0 && moisturePercentage <= 90 && totalPumpedWater < maxWaterLimit && currentHourOfDay >= 7 && currentHourOfDay < 19) {
     digitalWrite(waterPump, HIGH);
     Serial.println("Pump is ON");
     pumpStatus = true;
@@ -63,11 +76,26 @@ void loop() {
     pumpStatus = false;
   }
 
+  if (pumpStatus) {
+    unsigned long elapsedTime = currentMillis - pumpStartTime;
+    unsigned long pumpedWater = elapsedTime / 1000 * 28; // 28ml per second
+    totalPumpedWater += pumpedWater;
+    pumpStartTime = currentMillis;
+    Serial.print("Pumped Water: ");
+    Serial.print(totalPumpedWater);
+    Serial.println("ml");
+  } else {
+    pumpStartTime = 0;
+  }
+
   if (currentMillis - previousDataTime >= dataInterval) {
-    
-    sendSensorDataToGoogleSheets(moisturePercentage, pumpStatus);
+    sendSensorData(moisturePercentage, pumpStatus);
     previousDataTime = currentMillis;
   }
+  Serial.print("Hour Of the Day: ");
+  Serial.print(currentHourOfDay);
+  Serial.println("Hrs");
+  delay(3000);
 }
 
 void connectToWiFi() {
@@ -78,56 +106,64 @@ void connectToWiFi() {
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Successfully connected to : ");
+  Serial.print("Successfully connected to: ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+
   client.setInsecure();
 }
 
-void sendSensorDataToGoogleSheets(float moisture, bool pumpStatus) {
-  Serial.println("==========");
-  Serial.print("connecting to ");
-  Serial.println(host);
-  
-  //----------------------------------------Connect to Google host
-  if (!client.connect(host, httpsPort)) {
-    Serial.println("connection failed");
+time_t getNtpTime() {
+  while (!timeClient.update()) {
+    timeClient.forceUpdate();
+    delay(10);
+  }
+  return timeClient.getEpochTime();
+}
+
+void sendSensorData(float moisture, bool pumpStatus) {
+  client.setInsecure();
+
+  if (!client.connect(endpoint, httpsPort)) {
+    Serial.println("Connection failed");
     return;
   }
-  //----------------------------------------
 
-  //----------------------------------------Processing data and sending data
-  String string_moisture =  String(moisture);
-  // String string_temperature =  String(tem, DEC); 
-  String pump_status =  String(pumpStatus); 
-  String url = "/macros/s/" + sheet_id + "/exec?moisture=" + string_moisture + "&pump_status=" + pump_status;
-  Serial.print("requesting URL: ");
-  Serial.println(url);
+  String url_path = "/v1/functions/" + String(functionId) + "/executions";
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-         "Host: " + host + "\r\n" +
-         "User-Agent: BuildFailureDetectorESP8266\r\n" +
-         "Connection: close\r\n\r\n");
+  StaticJsonDocument<200> jsonDocument;
+  jsonDocument["data"] = "{\n\"moisture\": " + String(moisture) + ",\n\"pump_status\": " + (pumpStatus ? "true" : "false") + ",\n\"water_intake\": " + String(pumpedWater)+"\n}";
+  jsonDocument["async"] = false;
+  String requestData;
+  serializeJson(jsonDocument, requestData);
 
-  Serial.println("request sent");
-  while (client.connected()) {
-    String line = client.readStringUntil('\n');
-    if (line == "\r") {
-      Serial.println("headers received");
-      break;
+  String request = "POST " + url_path + " HTTP/1.1\r\n" +
+                   "Host: " + String(endpoint) + "\r\n" +
+                   "User-Agent: ESP8266\r\n" +
+                   "Content-Type: application/json\r\n" +
+                   "Content-Length: " + String(requestData.length()) + "\r\n" +
+                   "X-Appwrite-Project: " + String(project) + "\r\n" +
+                   "X-Appwrite-Key: " + String(appwriteKey) + "\r\n" +
+                   "Connection: close\r\n\r\n" +
+                   requestData + "\r\n";
+
+  client.print(request);
+
+  unsigned long timeout = millis() + 5000; // 5 seconds timeout
+  while (client.connected() && !client.available() && millis() < timeout) {
+    delay(1);
+  }
+
+  if (client.available()) {
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
     }
-  }
-  String line = client.readStringUntil('\n');
-  if (line.startsWith("{\"state\":\"success\"")) {
-    Serial.println("esp8266/Arduino CI successfull!");
   } else {
-    Serial.println("esp8266/Arduino CI has failed");
+    Serial.println("No response received");
   }
-  Serial.print("reply was : ");
-  Serial.println(line);
-  Serial.println("closing connection");
-  Serial.println("==========");
-  Serial.println();
+
+  client.stop();
 }
